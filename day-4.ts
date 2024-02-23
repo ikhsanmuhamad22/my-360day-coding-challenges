fb8935a4c5e338eb68202c519c7d07be22ca9543
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2024-02-23 14:20:25 +0700

    day 4/360

---
+++ day-4.ts
@@ -0,0 +1,33 @@
+// Day 4
+
+// Sentence Smash
+const smash = (words: string[]) => words.join(' ');
+
+console.log(smash(['hello', 'amazing', 'world']));
+
+// Number of People in the Bus
+// my solution
+const number = (busStops: any) => {
+  let naik = 0;
+  let turun = 0;
+  for (let i = 0; i < busStops.length; i++) {
+    naik += busStops[i][0];
+    turun += busStops[i][1];
+  }
+  return naik - turun;
+};
+
+// best solution
+const numberr = (busStops: any) =>
+  busStops.reduce((rem: any, [on, off]) => rem + on - off, 0);
+
+console.log(
+  number([
+    [3, 0],
+    [9, 1],
+    [4, 10],
+    [12, 2],
+    [6, 1],
+    [7, 10],
+  ])
+);
